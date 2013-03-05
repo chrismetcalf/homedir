2dfddf2d1aaa431bdc5ae2943dd7527744ed9678
Chris Metcalf <chris@chrismetcalf.net>
2013-03-05 15:40:20 -0800

    Tweaks to tmux-powerline

--- .tmux/powerline-themes/metcalf.sh
+++ .tmux/powerline-themes/metcalf.sh
@@ -17,7 +17,7 @@ TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLO
 
 TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_LEFTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}
 TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR:-$TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}
-
+TMUX_POWERLINE_SEG_NOW_PLAYING_MUSIC_PLAYER="spotify"
 
 # Format: segment_name background_color foreground_color [non_default_separator]
 
@@ -27,7 +27,7 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
     "hostname 33 0" \
     # "ifstat 30 255" \
     # "ifstat_sys 30 255" \
-    "lan_ip 24 255" \
+    # "lan_ip 24 255" \
     # "wan_ip 24 255" \
     # "vcs_branch 29 88" \
     # "vcs_compare 60 255" \
@@ -39,19 +39,20 @@ fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
   TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+    "timetrap 235 136"
     # "earthquake 3 0" \
     # "pwd 89 211" \
     # "mailcount 9 255" \
-    # "now_playing 234 37" \
+    "now_playing 234 37" \
     # "cpu 240 136" \
     "load 237 167" \
     # "tmux_mem_cpu_load 234 136" \
-    # "battery 137 127" \
+    "battery 137 127" \
     # "weather 37 255" \
     # "xkb_layout 125 117" \
     # "date_day 235 136" \
-    "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-    "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+    # "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+    # "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
     # "utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
     )
 fi
