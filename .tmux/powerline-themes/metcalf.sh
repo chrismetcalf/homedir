5ef66afdc9e7b7b627dd3553a300ad64dc062501
Chris Metcalf <chris@chrismetcalf.net>
2016-06-26 00:28:59 -0700

    Tmux config updates

--- .tmux/powerline-themes/metcalf.sh
+++ .tmux/powerline-themes/metcalf.sh
@@ -28,7 +28,7 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
     # "doing 235 136"
     # "ifstat 30 255" \
     # "ifstat_sys 30 255" \
-    # "lan_ip 24 255" \
+    "lan_ip 24 255" \
     # "wan_ip 24 255" \
     # "vcs_branch 29 88" \
     # "vcs_compare 60 255" \
