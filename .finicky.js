c6a44e122d2e293fa65d4b2676b3cba1c9a9f797
Chris Metcalf <chris@chrismetcalf.net>
2020-01-10 15:09:16 -0800

    New version of finicky

--- .finicky.js
+++ .finicky.js
@@ -1,40 +1,19 @@
-finicky.setDefaultBrowser('com.google.Chrome');
-
-// Open Spotify links in client
-finicky.onUrl(function(url, opts) {
-  if (url.match(/^https?:\/\/open\.spotify\.com/)) {
-    return {
-      bundleIdentifier: "com.spotify.client"
-    };
-  }
-});
-
-// Open Slack links in client
-// slack://channel?team=T0HMJC1K3&id=DAE2WRWFP&message=1526504560.000693
-//finicky.onUrl(function(url, opts) {
-//  if (url.match(/^https?:\/\/\w+\.slack\.com\/archives/)) {
-//    return {
-//      bundleIdentifier: "com.tinyspeck.slackmacgap"
-//    };
-//  }
-//});
-
-// Open some classes of link in Google Chrome Canary
-finicky.onUrl(function(url, opts) {
-  if (url.match(/^https?:\/\/\w+\.facebook\.com/)) {
-    return {
-      bundleIdentifier: "com.google.Chrome.canary"
-    };
-  }
-});
-
-// Rewrite all Bing links to DuckDuckGo instead
-finicky.onUrl(function(url, opts) {
-  var url = url.replace(
-    /^https?:\/\/www\.bing\.com\/search/,
-    "https://duckduckgo.com"
-  );
-  return {
-    url: url
-  };
-});
+module.exports = {
+  defaultBrowser: "/Applications/Google Chrome.app",
+  handlers: [
+    {
+      // Open apple.com and example.org urls in Safari
+      match: finicky.matchHostnames(["apple.com", "example.org"]),
+      browser: "Safari"
+    }
+    ,{
+      match: finicky.matchHostnames(["amperity.atlassian.com"]),
+      browser: "Jira"
+    }
+    ,{
+      // Open any url including the string "workplace" in Firefox
+      match: /workplace/,
+      browser: "Firefox"
+    }
+  ]
+};
